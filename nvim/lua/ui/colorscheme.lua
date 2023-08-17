7fef29fe0a2d5c5f1935daa817ccd7d15ef547e4
87YLu <chenyongchong.87@outlook.com>
2023-08-18 01:16:19 +0800

    update colorsheme

--- nvim/lua/ui/colorscheme.lua
+++ nvim/lua/ui/colorscheme.lua
@@ -9,11 +9,15 @@ end
 tokyonight.setup({
   style = 'night',
   transparent = true,
+  on_colors = function(colors)
+    colors.comment = '#737881'
+    colors.bg_visual = '#3a4150'
+  end,
 })
 
 -- transparent 设为 true 时必须指定 background_colour
 require('notify').setup({
-  background_colour = '#66ccff',
+  background_colour = '#414868',
 })
 
 vim.cmd('colorscheme tokyonight')
