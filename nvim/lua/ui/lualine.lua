64d79c86dce22f7cd5168babf8c03348d9b62f0b
87YLu <chenyongchong.87@outlook.com>
2023-10-01 21:41:12 +0800

    update lualine ui

--- nvim/lua/ui/lualine.lua
+++ nvim/lua/ui/lualine.lua
@@ -1,13 +1,13 @@
--- https://github.com/nvim-lualine/lualine.nvim
-local status_ok, lualine = pcall(require, 'lualine')
-
-if not status_ok then
-  vim.notify('lualine not found!')
-  return
-end
-
+local lualine = require('lualine')
 local input_mode = require('utils.input_mode')
 local system = require('utils.system')
+
+local function get_color(color)
+  local hl = vim.api.nvim_get_hl_by_name(color, true)
+  return {
+    fg = hl.foreground and string.format('#%06x', hl.foreground) or nil,
+  }
+end
 
 local function nav()
   local items = vim.b.coc_nav or {}
@@ -27,11 +27,12 @@ local function nav()
   return table.concat(t)
 end
 
-lualine.setup({
+local config = {
   options = {
-    component_separators = '|',
-    section_separators = { left = '', right = '' },
+    component_separators = '',
+    section_separators = '',
     disabled_filetypes = {
+      tabline = { 'alpha', 'neo-tree' },
       statusline = { 'alpha' },
       winbar = { 'neo-tree', 'alpha' },
       refresh = {
@@ -50,34 +51,122 @@ lualine.setup({
   extensions = { 'neo-tree', 'toggleterm' },
   sections = {
     lualine_a = {
-      { 'mode', separator = { right = '' }, right_padding = 2 },
+      {
+        'mode',
+        separator = { right = '' },
+        padding = { left = 2, right = 2 },
+        fmt = function(str)
+          return str:sub(1, 1)
+        end,
+      },
     },
-    lualine_b = {
-      'branch',
-      system.is_apple_silicon and input_mode.mode or nil,
-      { 'filename', separator = { right = '' } },
-    },
-    lualine_c = { 'diagnostics' },
+    lualine_b = {},
+    lualine_y = {},
+    lualine_z = {},
+    lualine_c = {},
     lualine_x = {},
-    lualine_y = {
-      {
-        'tabnine',
-        separator = { left = '' },
-      },
-      'filetype',
-      {
-        'fileformat',
-        symbols = {
-          unix = 'LF',
-          dos = 'CRLF',
-          mac = 'CR',
-        },
-      },
-      'encoding',
-      'progress',
-    },
-    lualine_z = {
-      'location',
-    },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_y = {},
+    lualine_z = {},
+    lualine_c = {},
+    lualine_x = {},
+  },
+}
+
+local function ins_left(component)
+  table.insert(config.sections.lualine_c, component)
+end
+
+local function ins_right(component)
+  table.insert(config.sections.lualine_x, component)
+end
+
+ins_left({
+  'filetype',
+  fmt = function()
+    return ' '
+  end,
+  padding = { left = 2, right = 0 },
+})
+
+ins_left({
+  'branch',
+  icon = '',
+  color = function()
+    return get_color('@tag')
+  end,
+  padding = { left = 0 },
+})
+
+if system.is_apple_silicon then
+  ins_left({
+    input_mode.mode,
+    icon = '󰓽',
+    color = function()
+      return get_color('@character')
+    end,
+  })
+end
+
+ins_left({
+  'diagnostics',
+  symbols = { error = ' ', warn = ' ', info = ' ', hint = '󰌵 ' },
+})
+
+ins_right({
+  'diff',
+  symbols = { added = ' ', modified = ' ', removed = ' ' },
+  diff_color = {
+    added = function()
+      return get_color('diffAdded')
+    end,
+    modified = function()
+      return get_color('diffChanged')
+    end,
+    removed = function()
+      return get_color('diffRemoved')
+    end,
   },
 })
+
+ins_right({
+  'tabnine',
+  fmt = function(str)
+    return str:gsub('tabnine starter', 'tabnine')
+  end,
+  color = function()
+    return get_color('@character.special')
+  end,
+})
+
+ins_right({
+  'encoding',
+  fmt = string.upper,
+  color = function()
+    return get_color('@repeat')
+  end,
+})
+
+ins_right({
+  'fileformat',
+  symbols = {
+    unix = 'LF',
+    dos = 'CRLF',
+    mac = 'CR',
+  },
+  color = function()
+    return get_color('@annotation')
+  end,
+})
+
+ins_right({
+  'location',
+  color = function()
+    return get_color('@string.regex')
+  end,
+})
+
+lualine.setup(config)
